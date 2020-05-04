b562950d5c47c174d4f8b8fe0b86ad10a6e23d0b
liangyehao <1094311509@qq.com>
2020-05-04 12:45:04 +0800

    非并发场景扣减库存

---
+++ src/main/java/com/liang/controller/RedisController.java
@@ -0,0 +1,37 @@
+package com.liang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+/**
+ * @author liangyehao
+ * @version 1.0
+ * @date 2020/5/4 12:20
+ * @content
+ */
+@RestController
+public class RedisController {
+
+    @Autowired
+    RedisTemplate<String,String> redisTemplate;
+
+    @GetMapping("/jkc")
+    public String test(){
+        int stock = Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get("stock")));
+        if (stock>0) {
+            int realStock = stock - 1;
+            redisTemplate.opsForValue().set("stock", String.valueOf(stock));
+            System.out.println("扣减成功,剩余库存: " + realStock);
+
+        }else{
+            System.out.println("扣减失败,库存不足.");
+        }
+        return "end";
+    }
+
+}
