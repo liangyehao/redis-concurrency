dccb701fa2c249b1c0f4339159d1adaf55c95fed
liangyehao <1094311509@qq.com>
2020-05-04 16:40:44 +0800

    3.redis实现分布式锁

--- src/main/java/com/liang/controller/RedisController.java
+++ src/main/java/com/liang/controller/RedisController.java
@@ -3,7 +3,6 @@ package com.liang.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
@@ -20,20 +19,35 @@ public class RedisController {
     @Autowired
     RedisTemplate<String,String> redisTemplate;
 
+    /**
+     * 测试减库存
+     *
+     * @return {@link String}
+     */
     @GetMapping("/jkc")
     public String test(){
-
-        synchronized (this){
-            int stock = Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get("stock")));
-            if (stock>0) {
-                int realStock = stock - 1;
-                redisTemplate.opsForValue().set("stock", String.valueOf(realStock));
-                System.out.println("扣减成功,剩余库存: " + realStock);
-
-            }else{
-                System.out.println("扣减失败,库存不足.");
-            }
+        // 1.加锁
+        String lockKey = "lockKey";
+        Boolean absent = Objects.requireNonNull(redisTemplate.opsForValue().setIfAbsent(lockKey, "liang"));
+        // 2.锁存在,直接返回,不执行减库存
+        if (!absent) {
+            System.out.println("无法获取资源,请重试...");
+            return "error";
         }
+        // 3.加锁成功,扣减库存
+        int stock = Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get("stock")));
+        if (stock>0) {
+            int realStock = stock - 1;
+            redisTemplate.opsForValue().set("stock", String.valueOf(realStock));
+            System.out.println("扣减成功,剩余库存: " + realStock);
+        }else{
+            System.out.println("扣减失败,库存不足.");
+        }
+
+        // 4.库存扣减完成,释放锁
+        redisTemplate.delete(lockKey);
+
+
         return "end";
     }
 
