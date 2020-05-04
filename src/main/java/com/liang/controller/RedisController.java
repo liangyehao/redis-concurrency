62bb9814b2cf6a4702f4a2303f72e8f48ba82ae7
liangyehao <1094311509@qq.com>
2020-05-04 16:44:22 +0800

    4.try catch finally 解决加锁后抛异常导致的死锁问题 (finally中释放锁)

--- src/main/java/com/liang/controller/RedisController.java
+++ src/main/java/com/liang/controller/RedisController.java
@@ -26,26 +26,30 @@ public class RedisController {
      */
     @GetMapping("/jkc")
     public String test(){
+
         // 1.加锁
         String lockKey = "lockKey";
-        Boolean absent = Objects.requireNonNull(redisTemplate.opsForValue().setIfAbsent(lockKey, "liang"));
-        // 2.锁存在,直接返回,不执行减库存
-        if (!absent) {
-            System.out.println("无法获取资源,请重试...");
-            return "error";
-        }
-        // 3.加锁成功,扣减库存
-        int stock = Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get("stock")));
-        if (stock>0) {
-            int realStock = stock - 1;
-            redisTemplate.opsForValue().set("stock", String.valueOf(realStock));
-            System.out.println("扣减成功,剩余库存: " + realStock);
-        }else{
-            System.out.println("扣减失败,库存不足.");
+        try {
+            Boolean absent = Objects.requireNonNull(redisTemplate.opsForValue().setIfAbsent(lockKey, "liang"));
+            // 2.锁存在,直接返回,不执行减库存
+            if (!absent) {
+                System.out.println("无法获取资源,请重试...");
+                return "error";
+            }
+            // 3.加锁成功,扣减库存
+            int stock = Integer.parseInt(Objects.requireNonNull(redisTemplate.opsForValue().get("stock")));
+            if (stock>0) {
+                int realStock = stock - 1;
+                redisTemplate.opsForValue().set("stock", String.valueOf(realStock));
+                System.out.println("扣减成功,剩余库存: " + realStock);
+            }else{
+                System.out.println("扣减失败,库存不足.");
+            }
+        } finally {
+            // 4.库存扣减完成,释放锁
+            redisTemplate.delete(lockKey);
         }
 
-        // 4.库存扣减完成,释放锁
-        redisTemplate.delete(lockKey);
 
 
         return "end";
